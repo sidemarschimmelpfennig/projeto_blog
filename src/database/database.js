460a2f5546517b0d3b9aae2988747d4459af50bc
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2025-06-28 01:00:40 -0300

    update env and config db

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,8 @@ const { Sequelize } = require("sequelize")
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect: 'mysql',   
-    timezone: '-03:00'
+    timezone: '-03:00',
+    port: process.env.DB_PORT
 } )
 
 
